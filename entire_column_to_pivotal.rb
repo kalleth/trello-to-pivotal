7d001e62f7b2bf566bbb94cd61853a485036a790
Tom Russell <tomrussell@notonthehighstreet.com>
2015-11-05 09:37:37 +0000

    Many changes. Use "Ready for Planning" as a gate.

--- entire_column_to_pivotal.rb
+++ entire_column_to_pivotal.rb
@@ -15,15 +15,49 @@ Trello.configure do |config|
 end
 
 LIST_REGEX = /Sprint \+1/
+ESTIMATE_REGEX = /\((\d)\)/
 
 board = Trello::Board.find(ENV['TRELLO_BOARD'])
 list = board.lists.select { |list| list.name =~ LIST_REGEX }.first
 
-csv_data = CSV.generate do |csv|
-  csv << %w(Title Type Description)
-  list.cards.each do |card|
-    csv << [card.name, 'feature', card.desc + "\n\nImported from: #{card.url}"]
+full_export = []
+max_tasks = 0
+
+list.cards.each do |card|
+  next unless card.labels.map(&:name).include?("Ready for Planning")
+
+  title = card.name
+  matches = title.match(ESTIMATE_REGEX)
+  estimate = matches.present? ? matches[1] : nil
+
+  output = [title, 'feature', card.desc + "\n\nImported from: #{card.url}", estimate]
+
+  # only support the first checklist for now
+  if card.checklists.any?
+    list = card.checklists.first
+    items = list.check_items
+
+    max_tasks = items.size if items.size > max_tasks
+    items.each do |item|
+      # translate back to raw from markdown?
+      output << item["name"]
+    end
   end
+
+  # warn because i can't import attachments
+  if card.attachments.any?
+    STDERR.puts "This card has attachments! Please import them manually from #{card.url}"
+  end
+
+  full_export << output
+end
+
+headings = %w(Title Type Description Estimate)
+headings = headings + max_tasks.times.map { "Task" }
+
+csv_data = CSV.generate do |csv|
+  csv << headings
+  full_export.each { |line| csv << line }
 end
 
 puts csv_data
